e98d19957dd8ed762a497cbeec419c7f0e984b14
taufiqhidy <46922081+taufiqhidy@users.noreply.github.com>
2021-08-14 22:06:33 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+pip install 
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+pip install mechanize
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+pip install requests bs4
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+pip2 install requests
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+pip2 install mechanize
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+apt install wget -y
+clear
+echo
+echo "         ______        _        "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+apt install ruby 
+echo
+echo "         ______        _         "
+echo "        |_____  |     |   \ /   "
+echo "        |       |____ |   / \   "
+echo "  ______|_________________________________________________ "
+echo
+gem install lolcat
+clear
+echo
+echo "         ______       _         " | lolcat
+echo "        |_____  |     |   \ /   " | lolcat
+echo "        |       |____ |   / \   " | lolcat
+echo "  ______|_________________________________________________ " | lolcat
+echo "    FLIX            YOUTUBE MILZU-TC TOTORIAL77            " | lolcat
+echo
+clear
+echo
+echo
+echo "       [√] install commando done..."
+echo
+echo "     type python2 MBF2D.py to continue "
+echo
