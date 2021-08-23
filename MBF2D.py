7a7fd589d4b7e4513d9279bdd6aa870c40897912
MILZUU TUTORIAL <46922081+taufiqhidy@users.noreply.github.com>
2021-08-23 12:36:31 +0700

    Update MBF2D.py

--- MBF2D.py
+++ MBF2D.py
@@ -67,7 +67,7 @@ def mbf2d():
     time.sleep(0.1)
 
     f=input("\n\033[90m╰>[>_<]👉 \033[1;93m")
-    if Milzu == "1":
+    if MilzuTC == "1":
          print("\033[1;94m=======================================>")
          mbasic = 'https://mbasic.facebook.com{}'
          global die,check,result, count
